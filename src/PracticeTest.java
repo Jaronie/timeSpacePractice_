3011ff56effb9c75f4dc5efa09f3350f5f88a036
Jaron <jayisaac6706@gmail.com>
2026-04-22 20:01:16 -0700

    freqNumSpaceEfficient test made

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -17,4 +17,16 @@ public class PracticeTest {
     // assert
     assertEquals(expected, actual, "tie break by choosing element that appears first.");
   }
+  @Test
+  public void testReturnFreqNumSpaceEfficient() {
+    //arrange
+    int[] nums = {2,3,2,1,2};
+    int expected = 2;
+
+    //act
+    int actual = Practice.mostCommonSpaceEfficient(nums);
+
+    //assert
+    assertEquals(expected, actual);
+  }
 }
